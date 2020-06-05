d8d2508c839caf8725b4bda1be85f93fe096566b
Eiqko <sarandaeiko@gmail.com>
2020-06-06 00:26:21 +0700

    Updates

---
+++ precache-manifest.4a1d5ebf3d53218d2a4f13c3b0b0b438.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de2e83bcebac4533a7cdc6ddb150988",
+    "url": "/commerce/index.html"
+  },
+  {
+    "revision": "5beab679483759f14663",
+    "url": "/commerce/static/css/main.97aa63ef.chunk.css"
+  },
+  {
+    "revision": "c018dd4ed30d159467b3",
+    "url": "/commerce/static/js/2.4db26ece.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/commerce/static/js/2.4db26ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5beab679483759f14663",
+    "url": "/commerce/static/js/main.0ae8cfa2.chunk.js"
+  },
+  {
+    "revision": "f7aae236cfeb6fd07e5a",
+    "url": "/commerce/static/js/runtime-main.76460285.js"
+  },
+  {
+    "revision": "efb34e724f59d14a1b9b307498d22c69",
+    "url": "/commerce/static/media/baju1.efb34e72.jpg"
+  },
+  {
+    "revision": "fce481678002bab8824e653a37ef4f27",
+    "url": "/commerce/static/media/baju2.fce48167.jpg"
+  },
+  {
+    "revision": "ae0f29605139c18368f348d0f8425316",
+    "url": "/commerce/static/media/cd1.ae0f2960.jpg"
+  },
+  {
+    "revision": "53e5bc273cc45c70d79d8ce18dd7d4da",
+    "url": "/commerce/static/media/cd2.53e5bc27.jpeg"
+  },
+  {
+    "revision": "ce43aae336fe664a0d2e278d16cd0251",
+    "url": "/commerce/static/media/cd3.ce43aae3.jpg"
+  },
+  {
+    "revision": "6eb45f9af5a083fc5ba8bb5e02702ddb",
+    "url": "/commerce/static/media/stadia.6eb45f9a.jpg"
+  }
+]);
